6944af685e22bde905ad92253d212ee989165f96
Shubham Jauhari <90859295+Jauhari-01@users.noreply.github.com>
2023-04-27 18:27:42 +0000

    Done assignment

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -1,5 +1,6 @@
 package com.driver;
 
+
 public class BankAccount {
 
     private String name;
@@ -7,25 +8,71 @@ public class BankAccount {
     private double minBalance;
 
     public BankAccount(String name, double balance, double minBalance) {
+        this.name= name;
+        this.balance= balance;
+        this.minBalance= minBalance;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public double getMinBalance() {
+        return minBalance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
     }
 
     public String generateAccountNumber(int digits, int sum) throws Exception{
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
+        boolean accGenerated= true;
 
-        return null;
-    }
+                if (9 * digits < sum) {
+                    accGenerated = false;
+                    throw new Exception("Account Number can not be generated");
+                }
+                    String number= "";
+                    number= createAccountNumber(digits, sum);
+                    return number;
+
+            }
 
     public void deposit(double amount) {
         //add amount to balance
-
+        balance+= amount;
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
+            if (balance - amount < minBalance) {
+                throw new Exception("Insufficient Balance");
+            } else balance -= amount;
+    }
+    public String createAccountNumber(int digits, int sum){
 
+        StringBuilder number= new StringBuilder("");
+
+        while(digits > 0){
+            if(sum > 9){
+                number.append("9");
+                sum -= 9;
+                digits--;
+            }
+            else{
+                number.append(String.valueOf(sum));
+                sum= 0;
+                digits--;
+            }
+        }
+        return String.valueOf(number);
     }
 
 }
